d1f562221cedffd58c9b7e1d2ab9d4c4513b6273
Mideli <muzipovanastia@gmail.com>
2023-11-26 20:56:49 +0200

    feat: add footer

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,7 @@
 import { createEl } from '../shared/createEl';
 import { createAbout } from '../widgets/about/create-about';
 import { createFavorite } from '../widgets/favorite/create-favorite';
+import { createFooter } from '../widgets/footer/create-footer';
 import { createHeader } from '../widgets/header/create-header';
 import { createHero } from '../widgets/hero/create-hero';
 import { createMobile } from '../widgets/mobile-app/create-mobile';
@@ -8,4 +9,4 @@ import './style.css';
 
 const container = createEl('div', 'page-container');
 document.body.append(container);
-container.append(createHeader(), createHero(), createFavorite(), createAbout(), createMobile());
+container.append(createHeader(), createHero(), createFavorite(), createAbout(), createMobile(), createFooter());
---
+++ src/widgets/footer/create-footer.js
@@ -0,0 +1,72 @@
+import { createEl } from '../../shared/createEl';
+import twitSrc from './assets/twitter.svg';
+import instaSrc from './assets/instagram.svg';
+import fbSrc from './assets/facebook.svg';
+import pin from './assets/pin-alt.svg';
+import phone from './assets/phone.svg';
+import clock from './assets/clock.svg';
+import './footer.css';
+
+export function createFooter() {
+  const footer = createEl('footer', 'footer');
+  const social = createEl('div', 'social-container');
+  const heading = createEl('h2', 'footer-heading');
+  heading.append('Sip, Savor, Smile.', createEl('p', 'cursive', 'It’s coffee time!'));
+  const socialWrapper = createEl('div', 'social-wrapper');
+
+  const twitterLink = createEl('a', 'social-link');
+  twitterLink.href = '';
+  const twitterIcon = createEl('img', 'social-icon');
+  twitterIcon.alt = 'twitter icon';
+  twitterIcon.src = twitSrc;
+  twitterLink.append(twitterIcon);
+
+  const instaLink = createEl('a', 'social-link');
+  instaLink.href = '';
+  const instaIcon = createEl('img', 'social-icon');
+  instaIcon.alt = 'insta icon';
+  instaIcon.src = instaSrc;
+  instaLink.append(instaIcon);
+
+  const fbLink = createEl('a', 'social-link');
+  fbLink.href = '';
+  const fbIcon = createEl('img', 'social-icon');
+  fbIcon.alt = 'fb icon';
+  fbIcon.src = fbSrc;
+  fbLink.append(fbIcon);
+
+  socialWrapper.append(twitterLink, instaLink, fbLink);
+  social.append(heading, socialWrapper);
+
+  const contacts = createEl('div', 'contacts-wrapper');
+  const contactsHeading = createEl('h3', 'contacts-heading', 'Contact us');
+  const contactsList = createEl('ul', 'contacts-list');
+
+  function createLI(icon, text, link) {
+    const li = createEl('li', 'contact-item');
+    const liIcon = createEl('img', 'contact-icon');
+    liIcon.alt = 'marker';
+    liIcon.src = icon;
+    if (link) {
+      const liLink = createEl('a', 'contact-link');
+      liLink.href = link;
+      liLink.target = 'blank';
+
+      li.append(liLink);
+      liLink.append(liIcon, text);
+    } else {
+      li.append(liIcon, text);
+    }
+
+    contactsList.append(li);
+  }
+
+  createLI(pin, '8558 Green Rd.,  LA', 'https://maps.app.goo.gl/22p2bNiNgS6XErLQ9');
+  createLI(phone, '+1 (603) 555-0123', 'tel:+1 (603) 555-0123');
+  createLI(clock, 'Mon-Sat: 9:00 AM – 23:00 PM');
+
+  contacts.append(contactsHeading, contactsList);
+
+  footer.append(social, contacts);
+  return footer;
+}
