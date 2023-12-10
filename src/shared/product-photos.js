8d469eee468443640efe1cf64670ee9482ff0d56
Mideli <muzipovanastia@gmail.com>
2023-12-10 16:39:55 +0200

    feat: add switching categories

--- src/shared/product-photos.js
+++ src/shared/product-photos.js
@@ -7,5 +7,40 @@ import coffee6 from './assets/coffee-6.jpg';
 import coffee7 from './assets/coffee-7.jpg';
 import coffee8 from './assets/coffee-8.jpg';
 
-const photos = [coffee1, coffee2, coffee3, coffee4, coffee5, coffee6, coffee7, coffee8];
-export default photos
+import dessert1 from './assets/dessert-1.jpg';
+import dessert2 from './assets/dessert-2.jpg';
+import dessert3 from './assets/dessert-3.jpg';
+import dessert4 from './assets/dessert-4.jpg';
+import dessert5 from './assets/dessert-5.jpg';
+import dessert6 from './assets/dessert-6.jpg';
+import dessert7 from './assets/dessert-7.jpg';
+import dessert8 from './assets/dessert-8.jpg';
+
+import tea1 from './assets/tea-1.jpg';
+import tea2 from './assets/tea-2.jpg';
+import tea3 from './assets/tea-3.jpg';
+import tea4 from './assets/tea-4.jpg';
+
+const photos = [
+  coffee1,
+  coffee2,
+  coffee3,
+  coffee4,
+  coffee5,
+  coffee6,
+  coffee7,
+  coffee8,
+  tea1,
+  tea2,
+  tea3,
+  tea4,
+  dessert1,
+  dessert2,
+  dessert3,
+  dessert4,
+  dessert5,
+  dessert6,
+  dessert7,
+  dessert8,
+];
+export default photos;
