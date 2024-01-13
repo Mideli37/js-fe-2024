839f546bb55551dd0f6ba5c09ba73cd643950e49
Mideli <muzipovanastia@gmail.com>
2024-01-13 17:26:27 +0200

    feat: change layout

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import './main.css';
+import { createEl } from './shared/create-el';
 import { Gallows } from './widgets/gallows/Gallows';
 import { Quiz } from './widgets/quiz/Quiz';
 
+const main = createEl('main', 'main');
 const gallow = new Gallows();
-document.body.append(gallow.container);
+document.body.append(main);
+
+const quizPartWrapper = createEl('div', 'quiz-part-wrapper');
 
 const quiz = new Quiz(0);
-document.body.append(quiz.container);
+main.append(gallow.container, quizPartWrapper);
+quizPartWrapper.append(quiz.container);
 
 quiz.reset();
 
@@ -18,3 +23,5 @@ quiz.openLetter(2);
 quiz.openLetter(3);
 quiz.openLetter(4);
 quiz.openLetter(6);
+
+gallow.addPart();
