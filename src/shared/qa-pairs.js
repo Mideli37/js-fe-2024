703f983fb1c634d0cb7db441a93ea4264b71ab2c
Mideli <muzipovanastia@gmail.com>
2024-01-13 15:55:45 +0200

    feat: add questions and answers

---
+++ src/shared/qa-pairs.js
@@ -0,0 +1,16 @@
+export const qaPairs = [
+  { id: 1, question: 'A popular snack consisting of a cooked sausege served in a sliced bun', answer: 'hotdog' },
+  { id: 2, question: 'The amount of money you pay to enter a place or event', answer: 'admission' },
+  { id: 3, question: 'A spooky attraction designed to scare visitors with various special effects', answer: 'hauntedhouse' },
+  { id: 4, question: 'A small structure or stand where goods or services are sold', answer: 'kiosk' },
+  { id: 5, question: 'A small item that is kept as a reminder of a place', answer: 'souvenir' },
+  { id: 6, question: 'A small, round object used as payment for rides or games', answer: 'token' },
+  { id: 7, question: 'A high-speed, looping ride that provides excitement and adrenaline rushes', answer: 'rollercoaster' },
+  { id: 8, question: 'A rotating platform with seats for people to ride on', answer: 'carousel' },
+  { id: 9, question: 'A famous and popular amusement park located in California', answer: 'disneyland' },
+  { id: 10, question: 'A small piece of paper that allows entry to a park or attraction', answer: 'ticket' },
+  { id: 11, question: 'A person who claims to predict the future and answer questions about your life', answer: 'fortuneteller' },
+  { id: 12, question: 'Items which can be won by playing games or participating in activities', answer: 'prizes' },
+  { id: 13, question: 'A large amusement park that follows a specific concept', answer: 'Themepark' },
+  { id: 14, question: 'A special pass that allows you to skip long lines and get on rides faster', answer: 'fastpass' },
+];
