2927943b2959ae5eb659707b69e604b025dc4286
Mideli <muzipovanastia@gmail.com>
2024-01-14 21:09:06 +0200

    refactor: rename style import

--- src/widgets/gallows/Gallows.js
+++ src/widgets/gallows/Gallows.js
@@ -1,5 +1,5 @@
 import { createEl } from '../../shared/create-el';
-import gallowStyles from './gallows.module.css';
+import styles from './gallows.module.css';
 
 import headImg from './assets/head.png';
 import lArmImg from './assets/hand-one.png';
@@ -9,27 +9,27 @@ import lLegImg from './assets/leg-one.png';
 import rLegImg from './assets/leg-two.png';
 
 export class Gallows {
-  container = createEl('div', gallowStyles.gallowContainer);
+  container = createEl('div', styles.gallowContainer);
   partsCount = 0;
 
   constructor() {
-    const pillow = createEl('div', gallowStyles.gallowPillow);
+    const pillow = createEl('div', styles.gallowPillow);
     this.bodyParts = [
-      [gallowStyles.head, headImg],
-      [gallowStyles.body, bodyImg],
-      [gallowStyles.leftArm, lArmImg],
-      [gallowStyles.rightArm, rArmImg],
-      [gallowStyles.leftLeg, lLegImg],
-      [gallowStyles.rightLeg, rLegImg],
+      [styles.head, headImg],
+      [styles.body, bodyImg],
+      [styles.leftArm, lArmImg],
+      [styles.rightArm, rArmImg],
+      [styles.leftLeg, lLegImg],
+      [styles.rightLeg, rLegImg],
     ];
     this.container.append(pillow);
 
     this.bodyParts = this.bodyParts.map((el) => {
-      let imgContainer = createEl('div', el[0]);
-      let img = createEl('img');
+      const imgContainer = createEl('div', el[0]);
+      const img = createEl('img');
       img.alt = 'body part';
       img.src = el[1];
-      imgContainer.classList.add(gallowStyles.hidden);
+      imgContainer.classList.add(styles.hidden);
       pillow.append(imgContainer);
       imgContainer.append(img);
       return imgContainer;
@@ -37,14 +37,14 @@ export class Gallows {
   }
 
   addPart() {
-    this.bodyParts[this.partsCount].classList.remove(gallowStyles.hidden);
+    this.bodyParts[this.partsCount].classList.remove(styles.hidden);
     this.partsCount += 1;
   }
 
   resetParts() {
     this.bodyParts.forEach((part) => {
-      if (!part.classList.contains(gallowStyles.hidden)) {
-        part.classList.add(gallowStyles.hidden);
+      if (!part.classList.contains(styles.hidden)) {
+        part.classList.add(styles.hidden);
       }
     });
     this.partsCount = 0;
