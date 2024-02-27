5ddfa428628b79d1726f63d69b535472d7176d5b
Mideli <muzipovanastia@gmail.com>
2024-02-27 22:00:32 +0200

    fix: add DocumentFragment to condition

--- src/helpers/safeQuerySelector.ts
+++ src/helpers/safeQuerySelector.ts
@@ -1,5 +1,5 @@
 export const safeQuerySelector = <E extends HTMLElement>(selector: string, baseElement: Node = document): E => {
-  if (!(baseElement instanceof Element || baseElement instanceof Document)) {
+  if (!(baseElement instanceof Element || baseElement instanceof Document ||  baseElement instanceof DocumentFragment)) {
     throw new Error(`Error: element is not instance of Element or Document`);
   }
   const elem = baseElement.querySelector<E>(selector);
