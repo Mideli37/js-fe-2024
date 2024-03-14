cde51cdad4083d0bd28065726fc39c0bdaba5564
Mideli37 <119811794+Mideli37@users.noreply.github.com>
2024-03-15 00:36:21 +0200

    feat: RSS-PZ-09 add dynamic sizing for cards

--- src/components/game/Game.ts
+++ src/components/game/Game.ts
@@ -2,8 +2,7 @@ import { createElement } from '@/helpers/create-element';
 import cardsInfo from '../../round-data-example.json';
 import { getWordCards, shuffleCards } from '@/helpers/get-word-cards';
 
-const containersStyle =
-  'flex flex-row justify-start gap-3 h-16 p-2 w-full bg-[#fcf3ee] border-2 border-[#7f1d1d] rounded-md';
+const containersStyle = 'flex flex-row justify-start h-16 p-2 w-full bg-[#fcf3ee] border-2 border-[#7f1d1d] rounded-md';
 
 export class Game {
   private gameWrapper = createElement('div', { className: 'h-full w-4/5 max-w-[800px] flex flex-col justify-around' });
--- src/helpers/get-word-cards.ts
+++ src/helpers/get-word-cards.ts
@@ -4,11 +4,13 @@ type WordElements = HTMLDivElement[];
 
 export function getWordCards(sentence: string): WordElements {
   const wordArr = sentence.split(' ');
+  const totalLength = sentence.length - (wordArr.length - 1);
   const elementArr = wordArr.map((word) => {
     const wrapper = createElement('div', {
       className:
-        'w-fit bg-[#ffe3d5] p-2 border-2 border-[#9e7775] rounded-md cursor-pointer duration-300 hover:bg-[#fff1eb]',
+        'card flex justify-center items-center text-center bg-[#ffe3d5] p-1 border-2 border-[#9e7775] rounded-md font-mono cursor-pointer duration-300 hover:bg-[#fff1eb]',
     });
+    wrapper.style.width = `${(word.length / totalLength) * 100}%`;
     wrapper.append(createElement('span', { textContent: word }));
     return wrapper;
   });
